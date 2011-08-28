0f2af7834fb2ed4caa18e0a39d6cd3bd754822cf
Radek Piekarz <radziupiekarz@gmail.com>
2011-08-28 11:35:57 +0200

    Moved to RubyMe

--- engines/helloWorld/config/routes.rb
+++ engines/helloWorld/config/routes.rb
@@ -1,24 +1,11 @@
-Praca::Application.routes.draw do
-	resources :users do
-    member do
-      get :following, :followers
-    end
-  end
+#Dummy::Application.routes.draw do
+Rails.application.routes.draw do
 
-  resources :users
-  resources :sessions,      :only => [:new, :create, :destroy]
-  resources :microposts,    :only => [:create, :destroy]
-  resources :relationships, :only => [:create, :destroy]
 
-  root :to => 'pages#home'
-  match '/signup',  :to => 'users#new'
-  match '/signin',  :to => 'sessions#new'
-  match '/signout', :to => 'sessions#destroy'
-	match '/contact', :to => 'pages#contact'
-  match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
-  get "pages/home"
-  get "pages/contact"
+  
+  match '/test',    :to => 'radek#about'
+  
+ # root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
